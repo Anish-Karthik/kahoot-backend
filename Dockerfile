5068992114babb97150de6f08917deb53bf2ffb6
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-06-07 14:12:16 +0530

    added docker

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-FROM eclipse-temurin:17
-
-LABEL mentainer="javaguides.net@gmail.com"
-
+# Use Maven to build the application
+FROM maven:3.8.5-openjdk-17 AS build
 WORKDIR /app
+COPY . .
+RUN mvn clean package -DskipTests
 
-COPY target/springboot-restful-webservices-0.0.1-SNAPSHOT.jar /app/springboot-restful-webservices.jar
-
-ENTRYPOINT ["java", "-jar", "springboot-restful-webservices.jar"]
+# Use OpenJDK slim image for runtime
+FROM openjdk:17.0.1-jdk-slim
+WORKDIR /app
+COPY --from=build /app/target/SMS-0.0.1-SNAPSHOT.jar kahoot.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "kahoot.jar"]
--- src/main/java/com/kahoot/kahoot/Controller/QuestionSetController.java
+++ src/main/java/com/kahoot/kahoot/Controller/QuestionSetController.java
@@ -1,21 +1,12 @@
 package com.kahoot.kahoot.Controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.kahoot.kahoot.Entity.QuestionSet;
 import com.kahoot.kahoot.Service.QuestionSetServicess;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -28,6 +19,8 @@ public class QuestionSetController {
     // Endpoint to fetch all questionSets
     @GetMapping
     public ResponseEntity<List<QuestionSet>> getAll() {
+
+        System.out.println("Hello");
         List<QuestionSet> questions = questionSetService.getAll();
         return ResponseEntity.ok(questions);
     }
